8579e1faee4d7b23cc847422c987210f29388718
me-ve <68286584+me-ve@users.noreply.github.com>
2022-04-28 22:59:55 +0200

    remove unused k-means generation code

--- Scrypts/k_means.py
+++ Scrypts/k_means.py
@@ -10,36 +10,8 @@ dataset = pd.read_csv('Database/Grouped_by_type.csv', header=0)
 
 X = dataset.iloc[:, 1:9].values
 
-"""
-# Use head() function to return the first 5 rows:
-dataset.head()
-# Assign values to the X and y variables:
-X = dataset.iloc[:, 1:9].values
-print("X___________")
-print(X)
-
-# define dataset
-#X, _ = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=4)
-print(X)
-# define the model
-model = KMeans(n_clusters=8)
-# fit the model
-model.fit(X)
-# assign a cluster to each example
-yhat = model.predict(X)
-# retrieve unique clusters
-clusters = unique(yhat)
-# create scatter plot for samples from each cluster
-for cluster in clusters:
-    # get row indexes for samples with this cluster
-    row_ix = where(yhat == cluster)
-    # create scatter of these samples
-    pyplot.scatter(X[row_ix, 0], X[row_ix, 1], X[row_ix, 2])
-# show the plot
-pyplot.show()
-"""
-#We always can divide on strict sience and non strict
-print(X)
+#Divide data into strict and non-strict science
+print(X) # 
 plt.scatter(X[:, 0], X[:, 1], s=50)
 plt.show()
 
