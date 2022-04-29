d06d1387ac7c4049c20c10a2e20e1c21f95b376b
me-ve <68286584+me-ve@users.noreply.github.com>
2022-04-29 21:09:00 +0200

    change loading data for boxplot, add documentation

--- Scrypts/boxplots.py
+++ Scrypts/boxplots.py
@@ -1,28 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-# subject percent ranges for each region
-data = [
-	[31.20, 31.37, 30.83, 30.57, 33.92, 39.49, 65.60, 49.96, 28.07],
-	[30.29, 30.97, 36.45, 30.84, 35.65, 41.05, 65.03, 44.49, 27.77],
-	[33.05, 36.91, 38.04, 31.21, 35.11, 33.74, 61.29, 46.15, 31.20],
-	[31.98, 33.78, 31.14, 31.63, 31.97, 33.37, 64.60, 54.33, 25.27],
-	[33.46, 32.78, 37.91, 34.82, 36.41, 33.13, 62.89, 41.91, 32.37],
-	[35.00, 38.67, 42.64, 34.68, 42.69, 44.50, 64.33, 49.29, 35.13],
-	[35.25, 37.49, 44.74, 38.57, 40.27, 40.65, 68.80, 51.77, 36.80],
-	[32.25, 27.82, 27.72, 31.54, 32.44, 33.82, 64.50, 48.21, 27.41],
-	[32.82, 35.50, 34.35, 30.91, 35.21, 31.64, 60.81, 44.63, 30.84],
-	[35.13, 42.04, 35.83, 33.59, 36.81, 43.42, 64.37, 52.55, 32.33],
-	[32.87, 36.41, 38.15, 34.14, 33.13, 38.31, 67.02, 43.11, 31.67],
-	[32.67, 34.38, 35.55, 34.28, 35.16, 37.45, 65.16, 51.96, 29.05],
-	[37.17, 36.39, 34.23, 31.18, 36.84, 33.47, 61.69, 49.73, 29.43],
-	[30.39, 30.40, 36.55, 29.96, 31.50, 30.51, 63.78, 46.82, 26.22],
-	[32.22, 32.81, 35.28, 32.15, 36.45, 39.17, 64.25, 52.65, 28.58],
-	[32.85, 33.54, 39.14, 30.48, 34.55, 35.51, 65.76, 54.57, 27.86]
-]
+# get data from csv file
+data = pd.read_csv('Database/tabela_danych.csv', header=0)
+data_subjects = data.iloc[:, 1:].values
+data_regions = data_subjects.transpose()
 
-# the same data but each subject is in the row instead of the column
-data_t = np.transpose(data)
 
 regions = [ # short regions names
 	"Dol",
@@ -87,8 +71,8 @@ def generate_boxplot(labels, plotdata):
 		patch.set_facecolor(color)
 	plt.show()
 
-# for regions
-generate_boxplot(regions, data)
+# generate for regions
+generate_boxplot(regions, data_regions)
 
-# for subjects
-generate_boxplot(subjects, np.transpose(data_t)) # for some weird reason data has to be transposed twice
+# generate for subjects
+generate_boxplot(subjects, data_subjects)
--- Scrypts/k_means.py
+++ Scrypts/k_means.py
@@ -6,9 +6,10 @@ from matplotlib import pyplot
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# get data from file
 dataset = pd.read_csv('Database/Grouped_by_type.csv', header=0)
 
-X = dataset.iloc[:, 1:9].values
+X = dataset.iloc[:, 1:].values # get data without labels
 
 # Divide data into strict and non-strict science
 # X - strict science
@@ -16,6 +17,7 @@ X = dataset.iloc[:, 1:9].values
 print(X) # output data for each region
 plt.scatter(X[:, 0], X[:, 1], s=50)
 plt.show()
+# each dot is different region
 
 # compute K-Means
 kmeans = KMeans(n_clusters=4)
@@ -25,3 +27,4 @@ plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
 centers = kmeans.cluster_centers_
 plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
 plt.show()
+# regions on the chart are grouped into 4 groups, which is shown by their color
